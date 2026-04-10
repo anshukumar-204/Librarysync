3f7afc7a6340f0b0dbce190133ee7104d947e9eb
Sandeep Sharma <sandeep08611@gmail.com>
2026-04-10 19:18:23 +0530

    Feature: Convert web app to installable PWA with auto-update service worker

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { store } from './store'
+import { registerSW } from 'virtual:pwa-register'
+
+// Register PWA Service Worker
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
